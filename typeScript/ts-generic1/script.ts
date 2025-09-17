d0843f30d404938c3a888dc8ef9c7a188d140ae1
Ireddy Yeshwanth <154913148+3lade@users.noreply.github.com>
2025-09-17 10:45:16 +0530

    script.ts

--- typeScript/ts-generic1/script.ts
+++ typeScript/ts-generic1/script.ts
@@ -1,37 +1,38 @@
-// exports {}
+exports {}
 
-// interface Task {
-//     name: string,
-//     priority: string
-// }
+interface Task {
+    name: string,
+    priority: string
+}
 
-// const taskName = document.getElementById('task') as HTMLInputElement;
-// const priorityName = document.getElementById('priority') as HTMLSelectElement;
-// const addBtn = document.getElementById('addTaskBtn') as HTMLButtonElement;
-// const errorMsg = document.getElementById('error-message') as HTMLDivElement;
-// const taskList = document.getElementById('task-list') as HTMLDivElement;
+const taskName = document.getElementById('task') as HTMLInputElement;
+const priorityName = document.getElementById('priority') as HTMLSelectElement;
+const addBtn = document.getElementById('addTaskBtn') as HTMLButtonElement;
+const errorMsg = document.getElementById('error-message') as HTMLDivElement;
+const taskList = document.getElementById('task-list') as HTMLDivElement;
 
-// addBtn.addEventListener('click', () => {
-//     const task = taskName.value;
-//     const priority = priorityName.value;
+addBtn.addEventListener('click', () => {
+    const task = taskName.value;
+    const priority = priorityName.value;
 
-//     if(!task || !priority)
-//     {
-//         errorMsg.textContent = 'Please enter a task and select a priority';
-//         return;
-//     }
+    if(!task || !priority)
+    {
+        errorMsg.textContent = 'Please enter a task and select a priority';
+        return;
+    }
 
-//     const li = document.createElement('li');
-//     li.textContent = `${task} [${priority}] - `
+    const li = document.createElement('li');
+    li.textContent = `${task} [${priority}] - `
     
-//     const btn = document.createElement('button');
-//     btn.textContent = 'Mark as Complete';
-//     btn.addEventListener('click', ()=> {
-//         btn.textContent = 'Completed';
-//         btn.disabled = true;
-//     })
+    const btn = document.createElement('button');
+    btn.textContent = 'Mark as Complete';
+    btn.addEventListener('click', ()=> {
+        btn.textContent = 'Completed';
+        btn.disabled = true;
+    })
 
-//     li.append(btn);
-//     taskList.append(li);
-// })
+    li.append(btn);
+    taskList.append(li);
+})
+
 
